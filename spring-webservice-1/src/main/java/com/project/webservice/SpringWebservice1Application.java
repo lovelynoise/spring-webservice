77eedbc426642061a978b72d3a833eb1a9b826fe
lake <lake@DESKTOP-5LVUURQ>
2018-04-02 14:10:08 +0900

    최초

---
+++ spring-webservice-1/src/main/java/com/project/webservice/SpringWebservice1Application.java
@@ -0,0 +1,12 @@
+package com.project.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebservice1Application.class, args);
+	}
+}
---
+++ spring-webservice-1/src/test/java/com/project/webservice/SpringWebservice1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.webservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebservice1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
